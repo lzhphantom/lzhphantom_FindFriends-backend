570d9072aadf1fdf23a03bb1c33e587669b5b03d
lzhphantom <lzhphantom2014@gmail.com>
2024-12-13 22:40:50 +0800

    2024年12月13日22:40:47

--- src/main/java/com/lzhphantom/lzhphantom_findfriendsbackend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/lzhphantom/lzhphantom_findfriendsbackend/exception/GlobalExceptionHandler.java
@@ -4,19 +4,23 @@ import com.lzhphantom.lzhphantom_findfriendsbackend.common.BaseResponse;
 import com.lzhphantom.lzhphantom_findfriendsbackend.common.ErrorCode;
 import com.lzhphantom.lzhphantom_findfriendsbackend.common.ResultUtils;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @Log4j2
 public class GlobalExceptionHandler {
     @ExceptionHandler(BusinessException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public BaseResponse<?> businessException(BusinessException e){
         log.error("BusinessException:{}", e.getMessage(),e);
         return ResultUtils.error(e.getCode(), e.getMessage());
     }
 
     @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public BaseResponse<?> runtimeException(RuntimeException e){
         log.error("runtimeException", e);
         return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
